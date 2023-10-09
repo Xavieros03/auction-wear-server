fbfed339e94aa1cf1a77ab41a3ba8f15db476823
Xavier Ortega <xaviersendra@hotmail.com>
2023-10-09 20:50:32 +0200

    REAL TIME AUCTION UPDATE

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ require("./db");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
-
+const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
+app.use(cors());
 
+const server = http.createServer(app);
 
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: ["my-custom-header"],
+        credentials: true,
+    },
+});
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
@@ -20,16 +32,16 @@ require("./config")(app);
 const indexRoutes = require("./routes/index.routes");
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require('./routes/user.routes')
-const {router, server} = require("./routes/auction.routes");
+const auctionRoutes = require('./routes/auction.routes')(io);
 const productRoutes = require("./routes/product.routes")
 
 app.use("/api", indexRoutes);
 app.use('/api/user', userRoutes)
-app.use("/api/auctions", router);
+app.use("/api/auctions", auctionRoutes);
 app.use('/api/products', productRoutes);
 app.use("/api/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
-module.exports = app;
+module.exports = { app, io, server };
--- routes/auction.routes.js
+++ routes/auction.routes.js
@@ -2,88 +2,66 @@ const express = require('express');
 const router = express.Router();
 const Auction = require('../models/Auction.model');
 const cron = require('node-cron');
+const io = require('../app').io;
 
+module.exports = (io) => {
+    cron.schedule('* * * * *', () => {
+        const currentTimestamp = new Date();
 
-const http = require('http');
-const server = http.createServer();
-
-const io = require('socket.io')(server);
-
-
-cron.schedule('* * * * *', () => {
-    let auctionsToStart = [];
-
-    
-    const currentTimestamp = new Date();
-
-    Auction.find({
-        status: 'scheduled',
-        scheduledStartTime: { $lte: currentTimestamp },
-    })
-        .then((foundAuctions) => {
-            auctionsToStart = foundAuctions;
-
-            const promises = auctionsToStart.map(async (auction) => {
-                auction.startTime = currentTimestamp;
-                auction.endTime = new Date(currentTimestamp.getTime() + 5 * 60 * 1000); 
-                auction.status = 'active';
-                await auction.save();
-
-                io.emit('auctionCreatedOrUpdated', auction); 
+        Auction.find({
+            status: 'scheduled',
+            scheduledStartTime: { $lte: currentTimestamp },
+        })
+            .then((foundAuctions) => {
+                io.emit('auctionCreatedOrUpdated', foundAuctions); 
+            })
+            .catch((error) => {
+                console.error('Error starting auctions:', error);
             });
-
-            return Promise.all(promises);
-        })
-        .then(() => {
-            console.log('Started', auctionsToStart.length, 'auctions.');
-        })
-        .catch((error) => {
-            console.error('Error starting auctions:', error);
-        });
-});
-
-router.get('/main', (req, res) => {
-    Auction.find()
-        .then((auctions) => {
-            res.json(auctions);
-        })
-        .catch((error) => {
-            console.error('Error fetching auctions:', error);
-            res.status(500).json({ message: 'Server error' });
-        });
-});
-
-router.post('/create', (req, res) => {
-    const { productId, startingBid, scheduledStartTime, userId } = req.body;
-
-    const sellerId = userId;
-
-    const currentTimestamp = new Date();
-    const startTime = currentTimestamp;
-    const endTime = new Date(currentTimestamp.getTime() + 5 * 60 * 1000); // Set the end time (5 minutes from start)
-
-
-    const newAuction = new Auction({
-        product: productId,
-        seller: sellerId,
-        startingBid,
-        scheduledStartTime,
-        startTime,
-        endTime,
-        participants: [userId],
     });
 
-    newAuction
-        .save()
-        .then((auction) => {
+    router.get('/main', (req, res) => {
+        Auction.find()
+            .then((auctions) => {
+                res.json(auctions);
+            })
+            .catch((error) => {
+                console.error('Error fetching auctions:', error);
+                res.status(500).json({ message: 'Server error' });
+            });
+    });
 
-            io.emit('auctionCreatedOrUpdated', auction);
-            res.status(201).json(auction);
-        })
-        .catch((error) => {
-            console.error('Error creating auction:', error);
-            res.status(500).json({ message: 'Server error' });
+    router.post('/create', (req, res) => {
+        const { productId, startingBid, scheduledStartTime, userId } = req.body;
+
+        const sellerId = userId;
+
+        const currentTimestamp = new Date();
+        const startTime = currentTimestamp;
+        const endTime = new Date(currentTimestamp.getTime() + 5 * 60 * 1000); 
+
+        const newAuction = new Auction({
+            product: productId,
+            seller: sellerId,
+            startingBid,
+            scheduledStartTime,
+            startTime,
+            endTime,
+            participants: [userId],
         });
-});
 
-module.exports = { router, server };
+        newAuction
+            .save()
+            .then((auction) => {
+                
+                io.emit('auctionCreatedOrUpdated', auction);
+                res.status(201).json(auction);
+            })
+            .catch((error) => {
+                console.error('Error creating auction:', error);
+                res.status(500).json({ message: 'Server error' });
+            });
+    });
+
+    return router;
+};
